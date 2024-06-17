87c1089272ad34ec328fb68ed8e2e996f5d923ba
haydenbspence <76969160+haydenbspence@users.noreply.github.com>
2024-06-17 10:33:39 -0400

    refactoring

--- hestia/app/crud/crud_concept.py
+++
@@ -1,8 +0,0 @@
-# app/crud/crud_concept.py
-
-from sqlalchemy.orm import Session
-from app.db.models import Concept
-
-def get_concept(db: Session, concept_id: int):
-    return db.query(Concept).filter(Concept.concept_id == concept_id).first()
-
